711fc1e01fcd3a2209a77989e766abe826ece848
khvilaboa <khvilaboa@esei.uvigo.es>
2016-03-13 21:49:54 +0100

    Basic database

---
+++ db.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+from google.appengine.ext import ndb
+
+
+class Subject(ndb.Model):
+    name = ndb.StringProperty(indexed=True)
+    description = ndb.StringProperty(indexed=False)
+    year = ndb.IntegerProperty(indexed=True)
+
+    @property
+    def tasks(self):
+        return Task.query(ancestor=self.key)
+
+    @property
+    def teachers(self):
+        return Task.query(ancestor=self.key)
+
+
+class Task(ndb.Model):
+    name = ndb.StringProperty(indexed=True)
+    percent = ndb.IntegerProperty(indexed=True)
+
+    # students = db.ListProperty(ndb.Key)
+
+
+class Teacher(ndb.Model):
+    email = ndb.StringProperty(indexed=False)
+
+    subjects = ndb.KeyProperty(kind="Subject", repeated=True)
+
+
+class Student(ndb.Model):
+    email = ndb.StringProperty(indexed=False)
+    dni = ndb.StringProperty(indexed=False)
+    name = ndb.StringProperty(indexed=False)
+
+    # subjects = db.ListProperty(ndb.Key)
