2537f6f649860c7e5df9eb7ccc0bd40ad27550f2
khvilaboa <khvilaboa@esei.uvigo.es>
2016-06-14 00:07:17 +0200

    File upload fix

--- profile.py
+++ profile.py
@@ -54,16 +54,16 @@ class ProfileHandler(base.BaseHandler):
             db.Teacher.addOrUpdate(self.getEmail(), self.request.get("name", None))
 
             self.redirect("/profile/%s" % self.getUserId())
-            return
 
-        avatar = self.request.get("img", None)
-        if avatar is not None:
-            #avatar = images.resize(avatar, 32, 32)
-            teacher = db.Teacher.getByEmail(self.getEmail())
-            teacher.setAvatar(avatar)
-            return
-
-        return
+        try:
+            avatar = self.request.get("img", None)
+            if avatar is not None:
+                #avatar = images.resize(avatar, 32, 32)
+                teacher = db.Teacher.getByEmail(self.getEmail())
+                teacher.setAvatar(avatar)
+                return
+        except:
+            pass
 
 class ProfileImageHandler(base.BaseHandler):
     def get(self):
